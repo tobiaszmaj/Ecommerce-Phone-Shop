5df008a48c8bf36bdda026fd56e17fb2c23c0027
Tobiasz Maj <tobiaszmaj1@gmail.com>
2019-05-24 00:39:15 +0200

    Updates

---
+++ precache-manifest.2cde3824605c648166a15b52e0e836be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/tobiaszmaj/Ecommerce-Phone-Shop/static/media/logo.9f7e506d.svg"
+  },
+  {
+    "revision": "17529d040a75960f80a5",
+    "url": "/tobiaszmaj/Ecommerce-Phone-Shop/static/js/runtime~main.2e3131fd.js"
+  },
+  {
+    "revision": "9885b7e966513a9d8402",
+    "url": "/tobiaszmaj/Ecommerce-Phone-Shop/static/js/main.95c98ed6.chunk.js"
+  },
+  {
+    "revision": "35e3d5b5b4c1ffefe10d",
+    "url": "/tobiaszmaj/Ecommerce-Phone-Shop/static/js/2.75b92cda.chunk.js"
+  },
+  {
+    "revision": "9885b7e966513a9d8402",
+    "url": "/tobiaszmaj/Ecommerce-Phone-Shop/static/css/main.eee51551.chunk.css"
+  },
+  {
+    "revision": "35e3d5b5b4c1ffefe10d",
+    "url": "/tobiaszmaj/Ecommerce-Phone-Shop/static/css/2.7bd305c9.chunk.css"
+  },
+  {
+    "revision": "755eb93ec5e55818e5fcfcd868b59924",
+    "url": "/tobiaszmaj/Ecommerce-Phone-Shop/index.html"
+  }
+];

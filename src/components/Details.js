6c3e7664b328b978f1d8a195432f0a2323a32e6e
Tobiasz Maj <tobiaszmaj1@gmail.com>
2020-05-13 22:02:25 +0200

    Changing README.md

--- src/components/Details.js
+++ src/components/Details.js
@@ -21,7 +21,7 @@ export default class Details extends Component {
                             {/* Product Info */}
                             <div className="row">
                                 <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
-                                    <img src={img} className="img-fluid" alt="product" />
+                                <img src={img} className="img-fluid" alt="product" />
                                 </div>
                                 {/* Product Text */}
                                 <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
